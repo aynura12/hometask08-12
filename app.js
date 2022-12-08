3fa25ae8fbe6bb346a0c39878258ca3ff7ddd46a
aynura12 <116623809+aynura12@users.noreply.github.com>
2022-12-08 13:24:35 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,97 +1,76 @@
-const Name = document.querySelector(".name");
-const surname = document.querySelector(".surname");
-const email = document.querySelector(".email");
-const password = document.querySelector(".password");
-const age = document.querySelector(".age");
-const sumbit = document.querySelector("#sumbit");
-const span1 = document.querySelector("#span1");
-const span2 = document.querySelector("#span2");
-const span3 = document.querySelector("#span3");
-const span4 = document.querySelector("#span4");
-const span5 = document.querySelector("#span5");
-const icon = document.querySelector(".fa-eye");
-const array=[];
+// let count = 0;
+// let sum = 0;
+// let avg=0;
+// Array.prototype.Avg= function () {
 
-const obj={
-    name:Name.value,
-    surname:surname.value,
-    email:email.value,
-    password:password.value,
-    age:age.value
-}
-array.push(obj)
-localStorage.setItem("array",JSON.stringify(array))
-
-Name.addEventListener("keyup", (e) => {
-    namefunc(e.target.value);
-});
-surname.addEventListener("keyup", (e) => {
-    surnamefunc(e.target.value);
-});
-email.addEventListener("keyup", (e) => {
-    emailfunc(e.target.value);
-});
-password.addEventListener("keyup", (e) => {
-    passfunc(e.target.value);
-});
-age.addEventListener("keyup", (e) => {
-    agefunc(e.target.value);
-});
+//     for (i = 0; i <this.length; i++) {
+//         sum += this[i];
+//         count++;
+//     }
+//     avg=sum/count;
+//     console.log(avg);
+// }  
+// let array = [1, 2, 3,2];
+// array.Avg()
 
 
-if (password.type = "password") {
-    icon.addEventListener("click", () => {
-        password.type = "text"
-    })
+const text = document.querySelector("#message-content");
+const duration = document.querySelector("#duration");
+const cancel = document.querySelector("#cancelable");
+const toast = document.querySelector("#toasts")
+const addtost = document.querySelector("#add-button");
+const clear = document.querySelector("#clear-button");
+const error = document.querySelector("#error-toast");
+const success = document.querySelector("#success-toast");
+const errorbutton = document.querySelector("#error");
+const successbutton = document.querySelector("#succes");
+const toastext = document.querySelector(".toast error-toast");
+const iscancel= document.querySelector(".cancel-botton");
+
+
+
+addtost.addEventListener("click", () => {
+    if ((text.value == "") &&(successbutton.checked == true)  && (cancel.checked==true )) {
+        toast.innerHTML = `<div class="toast error-toast">
+                <p>error</p> <button class="cancel-botton">X</button>
+            </div>`;
+    }
+    else if(errorbutton.checked == true && cancel.checked==true){
+        toast.innerHTML = `<div class="toast error-toast">
+        <p>${text.value}</p> <button class="cancel-botton">X</button>
+    </div>`;
+    }
+    else if ((text.value == "") &&(successbutton.checked == true)  && (cancel.checked==true )) {
+        toast.innerHTML = `<div class="toast success-toast">
+                <p>succes</p> <button class="cancel-botton">X</button>
+            </div>`;
+    }
+    else if((successbutton.checked == true)  && (cancel.checked==true )){
+        toast.innerHTML = `<div class="toast success-toast">
+        <p>succes</p> <button class="cancel-botton">X</button>
+    </div>`;
+    }
+
+    else if ((text.value == "") &&(successbutton.checked == true)  && (cancel.checked=false )) {
+        toast.innerHTML = `<div class="toast success-toast">
+                <p>succes</p>
+            </div>`;
+    }
+    else  {
+        toast.innerHTML = `<div class="toast success-toast">
+            <p>${text.value}</p>
+        </div>`;
+    }
+
+
+
+})
+
+clear.addEventListener("click", () => {
+    clearall()
+})
+
+function clearall() {
+    toast.innerText = ""
 }
 
-function namefunc(username) {
-    if (username.trim() == "") {
-        span1.innerText = "Invalid Input!";
-        return false;
-    }
-    else {
-        span1.innerText = "";
-        return true;
-    }
-}
-function surnamefunc(usersurname) {
-    if (usersurname.trim() == "") {
-        span2.innerText = "Invalid Input!";
-        return false;
-    }
-    else {
-        span2.innerText = "";
-        return true;
-    }
-}
-function emailfunc(useremail) {
-    if (useremail.trim() == "") {
-        span3.innerText = "Invalid Input!";
-        return false;
-    }
-    else {
-        span3.innerText = "";
-        return true;
-    }
-}
-function passfunc(userpass) {
-    if (userpass.trim() == "") {
-        span4.innerText = "Invalid Input!";
-        return false;
-    }
-    else {
-        span4.innerText = "";
-        return true;
-    }
-}
-function agefunc(userage) {
-    if (userage.trim() == "") {
-        span5.innerText = "Invalid Input!";
-        return false;
-    }
-    else {
-        span5.innerText = "";
-        return true;
-    }
-}
